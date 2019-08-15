d82ded9cdb029d873a34b54370202f891b84ce3d
q269482074 <49773808+q269482074@users.noreply.github.com>
2019-08-15 20:10:32 +0800

    后台商品页面完成
    后台商品页面完成

---
+++ application/admin/controller/Goods.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Goods extends Controller
+{
+    public function goodsList()
+    {
+        return $this->fetch();
+    }
+
+    public function addGoods()
+    {
+        return $this->fetch();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
