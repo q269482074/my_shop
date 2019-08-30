f36d85f3e30fe7a17fd1f417ab264ef22fe95bca
q269482074 <49773808+q269482074@users.noreply.github.com>
2019-08-30 12:59:29 +0800

    前台首页
    前台首页

---
+++ runtime/temp/c628b6f68ea2661d4d2b8bfcb152fde9.php
@@ -0,0 +1,45 @@
+<?php /*a:1:{s:75:"E:\phpStudy\PHPTutorial\WWW\my_shop\application\index\view\index\index.html";i:1567140854;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/static/index/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/static/index/layui/css/layui.css">
+    <link rel="stylesheet" href="/static/index/css/index.css">
+    <script src="/static/index/js/jquery-3.3.1.min.js"></script>
+    <script src="/static/index/layui/layui.js"></script>
+    <script src="/static/index/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <div class="header">
+            <div class="header-contriner">
+                <div class="cart">
+                    购物车
+                </div>
+                <div class="login">
+                    <div class="login-item">
+                        <a href="javascript:;">
+                            登录
+                        </a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                    </div>
+                    <div class="login-item">
+                        <a href="javascript:;">
+                            注册
+                        </a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                    </div>
+                    <div class="login-item">
+                        <a href="javascript:;">消息通知</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
