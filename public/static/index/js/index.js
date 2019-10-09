3c7161eea3747834f366addf9d00dc6033811ab0
q269482074 <49773808+q269482074@users.noreply.github.com>
2019-10-09 17:49:11 +0800

    首页楼层样式
    首页楼层样式

--- public/static/index/js/index.js
+++ public/static/index/js/index.js
@@ -7,4 +7,11 @@ $(function(){
         $(this).find('.search-best-content').css('height','0');
         $('.search-best-content').css('transition','0.3s');
     });
+
+    //选中商品样式变化
+    $('.floor-goods-item').mouseover(function(){
+        $(this).css('border','1px solid orange');
+    }).mouseout(function(){
+        $(this).css('border','none');
+    });
 })
